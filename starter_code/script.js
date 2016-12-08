378241efca52937fd6ccb170ad44a2b557357bf6
Deteriator <markgriffith009@gmail.com>
2016-12-08 23:17:07 +0000

    added search functionalit

--- starter_code/script.js
+++ starter_code/script.js
@@ -7,7 +7,16 @@ console.log(library.music.two_thousands.Lady_Gaga[0].song)
 
 $( document ).ready(function() {
 //Task 3: Have the words "Rolling in the Deep" appear on the HTML page in a div tag.    
-    $("body").append("<div>" + library.music.twenty_tens.Adele[0].song+"</div>");
+    $("#infobox").append( library.music.twenty_tens.Adele[0].song);
 //Task 4: (project) Create a site that can display songs from an artist specified by a user search.
+  var library= library.music;
+  $("#search_button").click(function(){
+    var user_search=$("#search_text").val;
+      //function to search through
+      for(i=0;user_search< library.length;i++)
+    $("#infobox").append(user_search);
+    $("#search_text").val("");
+    
 
+  });
 });
